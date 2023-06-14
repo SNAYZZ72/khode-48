d4303a5bee44d89eb9a3d39284081e2f108dd884
Nathan Sarmento <nathan.sarmento@etu.estia.fr>
2023-06-14 20:17:12 +0200

    header/footer v1.0.1

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -7,23 +7,23 @@ const Footer = () => {
   return (
     <Navbar fixed="bottom" style={{ backgroundColor: '#F24726' }}>
       <Nav className="flex-column">
-        <Nav.Link href="/contact" style={{ color: 'white', fontWeight: "500" }}>CONTACT</Nav.Link>
-        <Nav.Link href="/about" style={{ color: 'white', fontWeight: "500" }}>ABOUT US</Nav.Link>
-        <Nav.Link href="/terms" style={{ color: 'white', fontWeight: "500" }}>TERMS & CONDITIONS</Nav.Link>
+        <Nav.Link href="/contact" style={{ color: 'white', fontWeight: "500", marginLeft: '10px' }}>CONTACT</Nav.Link>
+        <Nav.Link href="/about" style={{ color: 'white', fontWeight: "500", marginLeft: '10px' }}>ABOUT US</Nav.Link>
+        <Nav.Link href="/terms" style={{ color: 'white', fontWeight: "500", marginLeft: '10px' }}>TERMS & CONDITIONS</Nav.Link>
       </Nav>
       <Nav className="flex-column">
-        <Nav.Link href="/faq" style={{ color: 'white', fontWeight: "500" }}>FAQ</Nav.Link>
-        <Nav.Link href="/careers" style={{ color: 'white', fontWeight: "500" }}>CAREERS</Nav.Link>
-        <Nav.Link href="/country" style={{ color: 'white', fontWeight: "500" }}>CHANGE COUNTRY</Nav.Link>
+        <Nav.Link href="/faq" style={{ color: 'white', fontWeight: "500", marginLeft: '20px' }}>FAQ</Nav.Link>
+        <Nav.Link href="/careers" style={{ color: 'white', fontWeight: "500", marginLeft: '20px' }}>CAREERS</Nav.Link>
+        <Nav.Link href="/country" style={{ color: 'white', fontWeight: "500", marginLeft: '20px' }}>CHANGE COUNTRY</Nav.Link>
       </Nav>
-      <Nav>
-        <Nav.Link href="https://twitter.com" style={{ color: 'white', fontSize: '45px', margin: '10px' }}>
+      <Nav className="flex-column">
+        <Nav.Link href="https://twitter.com" style={{ color: 'white', fontSize: '20px', marginLeft: '20px' }}>
           <FontAwesomeIcon icon={faTwitter} />
         </Nav.Link>
-        <Nav.Link href="https://www.facebook.com" style={{ color: 'white', fontSize: '45px', margin: '10px' }}>
+        <Nav.Link href="https://www.facebook.com" style={{ color: 'white', fontSize: '20px', marginLeft: '20px' }}>
           <FontAwesomeIcon icon={faFacebook} />
         </Nav.Link>
-        <Nav.Link href="https://www.instagram.com" style={{ color: 'white', fontSize: '45px', margin: '10px' }}>
+        <Nav.Link href="https://www.instagram.com" style={{ color: 'white', fontSize: '20px', marginLeft: '20px' }}>
           <FontAwesomeIcon icon={faInstagram} />
         </Nav.Link>
       </Nav>
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -6,13 +6,13 @@ const Header = () => {
   return (
     <Navbar sticky="top" expand="lg">
       <Container>
-        <Navbar.Brand><img src={logo} alt="Logo" style={{ width: '200px', height: 'auto' }}/></Navbar.Brand>
+        <Navbar.Brand><img src={logo} alt="Logo" style={{ width: '200px', height: 'auto', position: 'fixed', left: '10px', top: '10px' }}/></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/" style={{ color: 'black', fontWeight: "500", marginLeft: '10px', marginRight: '10px' }}>HOME</Nav.Link>
-            <Nav.Link href="/contact" style={{ color: 'black', fontWeight: "500", marginLeft: '10px', marginRight: '10px' }}>CONTACT</Nav.Link>
-            <Nav.Link href="/about" style={{ color: 'black', fontWeight: "500", marginLeft: '10px', marginRight: '10px' }}>ABOUT US</Nav.Link>
+            <Nav.Link href="/" style={{ color: 'black', fontWeight: "650", marginLeft: '10px', marginRight: '10px', marginTop: '10px' }}>HOME</Nav.Link>
+            <Nav.Link href="/contact" style={{ color: 'black', fontWeight: "650", marginLeft: '10px', marginRight: '10px', marginTop: '10px'  }}>CONTACT</Nav.Link>
+            <Nav.Link href="/about" style={{ color: 'black', fontWeight: "650", marginLeft: '10px', marginRight: '10px', marginTop: '10px'  }}>ABOUT US</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
