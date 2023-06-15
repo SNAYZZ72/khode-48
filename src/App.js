37dd44a594bd667eec0afebdbe2d194fc29ce9e0
Nathan Sarmento <nathan.sarmento@etu.estia.fr>
2023-06-15 18:19:11 +0200

    changelanguages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './i18n';
+
 // import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,17 +19,19 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 export default function App() {
   return (
-    <div className="app-container">
-      <Router>
-        <div className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-          </Routes>
-        </div>
-        <Footer />
-      </Router>
-    </div>
+    <I18nextProvider i18n={i18n}>
+      <div className="app-container">
+        <Router>
+          <div className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    </I18nextProvider>
   );
 }
 root.render(<App />);
