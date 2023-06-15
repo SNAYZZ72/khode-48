9fbe09e5ddd2426c67897f92d19566e6644d6034
Matthieu <matthieu.picot@etu.estia.fr>
2023-06-15 19:14:09 +0200

    setup firebase

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initialiseApp } from 'firebase/app';   // import initialiseApp from 'firebase/app';
+
+const firebaseApp = initialiseApp({
+    apiKey: "AIzaSyCuBAIQ0gGIO4ts4ucaTMmneCoObhhH1s4",
+  authDomain: "kode-48-373e9.firebaseapp.com",
+  projectId: "kode-48-373e9",
+  storageBucket: "kode-48-373e9.appspot.com",
+  messagingSenderId: "180923264231",
+  appId: "1:180923264231:web:3c9ab9546d2805441a0642",
+  measurementId: "G-1XXYPWBFHM"
+});
