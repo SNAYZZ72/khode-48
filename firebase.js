44d38c684c7a95836dd6315921c7f71eb0e35c4a
Matthieu <matthieu.picot@etu.estia.fr>
2023-06-16 11:53:07 +0200

    database ca marche et le site est upload

--- firebase.js
+++ firebase.js
@@ -1,11 +1,30 @@
-import { initialiseApp } from 'firebase/app';   // import initialiseApp from 'firebase/app';
+const { initializeApp } = require('firebase/app');
+const { getDatabase, ref, set } = require('firebase/database');
 
-const firebaseApp = initialiseApp({
+const firebaseConfig = {
     apiKey: "AIzaSyCuBAIQ0gGIO4ts4ucaTMmneCoObhhH1s4",
-  authDomain: "kode-48-373e9.firebaseapp.com",
-  projectId: "kode-48-373e9",
-  storageBucket: "kode-48-373e9.appspot.com",
-  messagingSenderId: "180923264231",
-  appId: "1:180923264231:web:3c9ab9546d2805441a0642",
-  measurementId: "G-1XXYPWBFHM"
-});
+    authDomain: "kode-48-373e9.firebaseapp.com",
+    databaseURL: "https://kode-48-373e9-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "kode-48-373e9",
+    storageBucket: "kode-48-373e9.appspot.com",
+    messagingSenderId: "180923264231",
+    appId: "1:180923264231:web:3c9ab9546d2805441a0642",
+    measurementId: "G-1XXYPWBFHM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+function writeUserData(userId, name, email, imageUrl) {
+    const db = getDatabase();
+    const reference = ref(db, 'users/' + userId);
+
+    set(reference, {
+        username: name,
+        email: email,
+        profile_picture : imageUrl
+    });
+    console.log('success');
+}
+
+writeUserData('matthieu', 'matthlamenace', 'emailtest@mail.com', 'imageURL');
