0b23eade8d05cba202ce58627e2acbd7d6ca2d03
SNAYZZ72 <103134452+SNAYZZ72@users.noreply.github.com>
2023-06-16 11:02:54 +0200

    required

--- src/components/Pages/RegisteredYouth.js
+++ src/components/Pages/RegisteredYouth.js
@@ -4,9 +4,7 @@ import Header from '../common/Header';
 
 const RegisterPage = () => {
     const { t } = useTranslation();
-    const [fullName, setFullName] = useState('');
     const [firstName, setFirstName] = useState('');
-    const [middleName, setMiddleName] = useState('');
     const [lastName, setLastName] = useState('');
     const [gender, setGender] = useState('');
     const [dateOfBirth, setDateOfBirth] = useState('');
@@ -21,16 +19,8 @@ const RegisterPage = () => {
     const [educationalLevel, setEducationalLevel] = useState('');
     const [cvFile, setCvFile] = useState(null);
 
-    const handleFullNameChange = (e) => {
-        setFullName(e.target.value);
-    };
-
     const handleFirstNameChange = (e) => {
         setFirstName(e.target.value);
-    };
-
-    const handleMiddleNameChange = (e) => {
-        setMiddleName(e.target.value);
     };
 
     const handleLastNameChange = (e) => {
@@ -91,9 +81,7 @@ const RegisterPage = () => {
         // Perform form submission logic here
         // You can send the form data to a backend server or perform any other actions
         // Reset form fields after submission if needed
-        setFullName('');
         setFirstName('');
-        setMiddleName('');
         setLastName('');
         setGender('');
         setDateOfBirth('');
@@ -116,55 +104,117 @@ const RegisterPage = () => {
                 <h1>{t('Young People Register')}</h1>
             </div>
             <div className="container">
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="row mb-3">
                         <div className="col">
                             <label htmlFor="firstName">{t('firstName')}</label>
-                            <input type="text" className="form-control" id="firstName" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="firstName"
+                                value={firstName}
+                                onChange={handleFirstNameChange}
+                                required
+                            />
                         </div>
                         <div className="col">
                             <label htmlFor="lastName">{t('lastName')}</label>
-                            <input type="text" className="form-control" id="lastName" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="lastName"
+                                value={lastName}
+                                onChange={handleLastNameChange}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col">
                             <label htmlFor="gender">{t('gender')}</label>
-                            <select className="form-control" id="gender">
-                                    <option value="">{t('selectGender')}</option>
-                                    <option value="female">{t('female')}</option>
-                                    <option value="male">{t('male')}</option>
-                                    <option value="nonBinary">{t('nonBinary')}</option>
-                                    <option value="preferNotToSay">{t('preferNotToSay')}</option>
+                            <select
+                                className="form-control"
+                                id="gender"
+                                value={gender}
+                                onChange={handleGenderChange}
+                                required
+                            >
+                                <option value="">{t('selectGender')}</option>
+                                <option value="female">{t('female')}</option>
+                                <option value="male">{t('male')}</option>
+                                <option value="nonBinary">{t('nonBinary')}</option>
+                                <option value="preferNotToSay">{t('preferNotToSay')}</option>
                             </select>
                         </div>
                         <div className="col">
                             <label htmlFor="dateOfBirth">{t('dateOfBirth')}</label>
-                            <input type="date" className="form-control" id="dateOfBirth" />
+                            <input
+                                type="date"
+                                className="form-control"
+                                id="dateOfBirth"
+                                value={dateOfBirth}
+                                onChange={handleDateOfBirthChange}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col-md-8">
                             <label htmlFor="address">{t('address')}</label>
-                            <input type="text" className="form-control" id="address" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="address"
+                                value={address}
+                                onChange={handleAddressChange}
+                                required
+                            />
                         </div>
                         <div className="col-md-2">
                             <label htmlFor="city">{t('city')}</label>
-                            <input type="text" className="form-control" id="city" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="city"
+                                value={city}
+                                onChange={handleCityChange}
+                                required
+                            />
                         </div>
                         <div className="col-md-2">
                             <label htmlFor="postalCode">{t('postalCode')}</label>
-                            <input type="text" className="form-control" id="postalCode" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="postalCode"
+                                value={postalCode}
+                                onChange={handlePostalCodeChange}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col">
                             <label htmlFor="email">{t('email')}</label>
-                            <input type="email" className="form-control" id="email" />
+                            <input
+                                type="email"
+                                className="form-control"
+                                id="email"
+                                value={email}
+                                onChange={handleEmailChange}
+                                required
+                            />
                         </div>
                         <div className="col">
                             <label htmlFor="phoneNumber">{t('phoneNumber')}</label>
-                            <input type="tel" className="form-control" id="phoneNumber" />
+                            <input
+                                type="tel"
+                                className="form-control"
+                                id="phoneNumber"
+                                value={phoneNumber}
+                                onChange={handlePhoneNumberChange}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="row mb-3">
@@ -176,6 +226,7 @@ const RegisterPage = () => {
                                 id="password"
                                 value={password}
                                 onChange={handlePasswordChange}
+                                required
                             />
                         </div>
                         <div className="col">
@@ -186,13 +237,20 @@ const RegisterPage = () => {
                                 id="confirmPassword"
                                 value={confirmPassword}
                                 onChange={handleConfirmPasswordChange}
+                                required
                             />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <div className="col">
                             <label htmlFor="educationalLevel">{t('educationalLevel')}</label>
-                            <select className="form-control" id="educationalLevel">
+                            <select
+                                className="form-control"
+                                id="educationalLevel"
+                                value={educationalLevel}
+                                onChange={handleEducationalLevelChange}
+                                required
+                            >
                                 <option value="primary">{t('primary')}</option>
                                 <option value="secondary">{t('secondary')}</option>
                                 <option value="higherEducation">{t('higherEducation')}</option>
@@ -200,22 +258,50 @@ const RegisterPage = () => {
                         </div>
                         <div className="col">
                             <label htmlFor="lastName">{t('lastName')}</label>
-                            <input type="text" className="form-control" id="lastName" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="lastName"
+                                value={lastName}
+                                onChange={handleLastNameChange}
+                                required
+                            />
                         </div>
                     </div>
                     <div className="form-group">
                         <label htmlFor="educationalLevel">{t('educationalLevel')}</label>
-                        <select className="form-control" id="educationalLevel">
+                        <select
+                            className="form-control"
+                            id="educationalLevel"
+                            value={educationalLevel}
+                            onChange={handleEducationalLevelChange}
+                            required
+                        >
                             <option value="primary">{t('primary')}</option>
                             <option value="secondary">{t('secondary')}</option>
                             <option value="higherEducation">{t('higherEducation')}</option>
                         </select>
                     </div>
+                    <br />
                     <div className="form-group">
                         <label htmlFor="cv">{t('uploadCV')}</label>
-                        <input type="file" className="form-control-file" id="cv" onChange={handleFileChange} />
+                        <input
+                            type="file"
+                            className="form-control-file"
+                            id="cv"
+                            onChange={handleFileChange}
+                            required
+                        />
                     </div>
-                    <button type="submit" className="btn btn-primary" style={{ backgroundColor: '#F24726', borderColor: '#F24726' }}>{t('registerButton')}</button>
+                    <br />
+                    <button
+                        type="submit"
+                        className="btn btn-primary"
+                        style={{ backgroundColor: '#F24726', borderColor: '#F24726' }}
+                        required
+                    >
+                        {t('registerButton')}
+                    </button>
                 </form>
             </div>
         </div>
