34532f17fbaf4ba71edf9cb340f4c97ce5df7d40
Nathan Sarmento <nathan.sarmento@etu.estia.fr>
2023-07-06 12:13:25 +0200

    zezezez

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -12,25 +12,6 @@ const Footer = () => {
         <Nav.Link href="/about" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginLeft: '10px' }}>ABOUT US</Nav.Link>
         <Nav.Link href="/terms" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginLeft: '10px' }}>TERMS & CONDITIONS</Nav.Link>
       </Nav>
-      <Nav className="align-items-center">
-        {/* Contenu de la dernière colonne */}
-        <Nav.Link href="https://twitter.com" target="_blank" style={{ color: 'white', fontSize: '40px' }}>
-          <FontAwesomeIcon icon={faTwitter} />
-        </Nav.Link>
-        <Nav.Link href="https://www.facebook.com" target="_blank" style={{ color: 'white', fontSize: '40px' }}>
-          <FontAwesomeIcon icon={faFacebook} />
-        </Nav.Link>
-        <Nav.Link href="https://www.instagram.com" target="_blank" style={{ color: 'white', fontSize: '40px' }}>
-          <FontAwesomeIcon icon={faInstagram} />
-        </Nav.Link>
-      </Nav>
-      <Nav className="flex-column text-end">
-        {/* Contenu de la colonne centrale */}
-        <Nav.Link href="/FAQ" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginRight: '10px' }}>FAQ</Nav.Link>
-        <Nav.Link href="/careers" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginRight: '10px' }}>CAREERS</Nav.Link>
-        <Nav.Link href="/country" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginRight: '10px' }}>CHANGE COUNTRY</Nav.Link>
-      </Nav>
-
     </Navbar>
   );
 }
