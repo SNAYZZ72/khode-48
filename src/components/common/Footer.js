0a6713691652967f80a10a91bf32e69315c5262b
Nathan Sarmento <nathan.sarmento@etu.estia.fr>
2023-07-11 11:50:45 +0200

    zdzdzd

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
@@ -12,17 +10,30 @@ const Footer = () => {
         <Nav.Link href="/about" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginLeft: '10px' }}>About us</Nav.Link>
         <Nav.Link href="/terms" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginLeft: '10px' }}>Terms & conditions</Nav.Link>
       </Nav>
-      {/* <Nav className="align-items-center">
-        <Nav.Link href="https://twitter.com" target="_blank" style={{ color: 'white', fontSize: '40px' }}>
-          <FontAwesomeIcon icon={faTwitter} />
-        </Nav.Link>
-        <Nav.Link href="https://www.facebook.com" target="_blank" style={{ color: 'white', fontSize: '40px' }}>
-          <FontAwesomeIcon icon={faFacebook} />
-        </Nav.Link>
-        <Nav.Link href="https://www.instagram.com" target="_blank" style={{ color: 'white', fontSize: '40px' }}>
-          <FontAwesomeIcon icon={faInstagram} />
-        </Nav.Link>
-      </Nav> */}
+      <Nav className="align-items-center">
+        <div className="row">
+          <div className="col-md-3 text-center">
+            <Nav.Link href="https://twitter.com" target="_blank">
+              <img src="bfa.png" alt="Twitter" className="footer-icon" style={{ height:'40px', width: 'auto' }} />
+            </Nav.Link>
+          </div>
+          <div className="col-md-3 text-center">
+            <Nav.Link href="https://www.facebook.com" target="_blank">
+              <img src="kultiva.svg" alt="Facebook" className="footer-icon" style={{ height:'50px', width: 'auto' }} />
+            </Nav.Link>
+          </div>
+          <div className="col-md-3 text-center">
+            <Nav.Link href="https://www.instagram.com" target="_blank">
+              <img src="c2b.png" alt="Instagram" className="footer-icon" style={{ height:'40px', width: 'auto' }} />
+            </Nav.Link>
+          </div>
+          <div className="col-md-3 text-center">
+            <Nav.Link href="https://www.instagram.com" target="_blank">
+              <img src="bigel.png" alt="Instagram" className="footer-icon" style={{ height:'40px', width: 'auto' }} />
+            </Nav.Link>
+          </div>
+        </div>
+      </Nav>
       <Nav className="flex-column text-end">
         {/* Contenu de la colonne centrale */}
         <Nav.Link href="/FAQ" style={{ color: 'white', fontWeight: '500', fontSize: '15px', marginRight: '10px' }}>Aviso legal</Nav.Link>
