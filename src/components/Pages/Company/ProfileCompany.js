65b6964caba6f04fcb18cf4078c0ce9f6c1d35c9
Nathan Sarmento <nathan.sarmento@etu.estia.fr>
2023-07-12 12:27:49 +0200

    edit company marche à moitié

--- src/components/Pages/Company/ProfileCompany.js
+++ src/components/Pages/Company/ProfileCompany.js
@@ -193,7 +193,7 @@ const ProfileCompany = () => {
                     <form>
                         <div className="row mb-3">
                             <div className="col">
-                                <label htmlFor="city">{t('city')}</label>
+                                <label>{t('city')}</label>
                                 <input
                                     className="form-control"
                                     type="text"
@@ -207,10 +207,25 @@ const ProfileCompany = () => {
                                 )}
 
                             </div>
+                            <div className="col">
+                                <label>{t('postalCode')}</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    id="postalCode"
+                                    name="postalCode"
+                                    value={companyFormData.postalCode}
+                                    onChange={handleInputChange}
+                                />
+                                {companyFormErrors.postalCode && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+
+                            </div>
                         </div>
                         <div className="row mb-3">
                             <div className="col">
-                                <label htmlFor="aboutme">About me</label>
+                                <label htmlFor="aboutme">{t('aboutCompany')}</label>
                                 <textarea
                                     className="form-control"
                                     id="information"
@@ -221,11 +236,147 @@ const ProfileCompany = () => {
                                 {companyFormErrors.information && (
                                     <div className="invalid-feedback">About me field is required</div>
                                 )}
+
                             </div>
                         </div>
                         <div className="row mb-3">
                             <div className="col">
-                                <label>Profile picture</label>
+                                <label>{t('contactFirstName')}</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    id="contactFirstName"
+                                    name="contactFirstName"
+                                    value={companyFormData.contactFirstName}
+                                    onChange={handleInputChange}
+                                />
+                                {companyFormErrors.contactFirstName && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                            <div className="col">
+                                <label>{t('contactLastName')}</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    id="contactLastName"
+                                    name="contactLastName"
+                                    value={companyFormData.contactLastName}
+                                    onChange={handleInputChange}
+                                />
+                                {companyFormErrors.contactLastName && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                            <div className="col">
+                                <label>{t('contactRole')}</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    id="contactRole"
+                                    name="contactRole"
+                                    value={companyFormData.contactRole}
+                                    onChange={handleInputChange}
+                                />
+                                {companyFormErrors.contactRole && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <label>{t('phoneNumber')}</label>
+                                <input
+                                    className="form-control"
+                                    type="text"
+                                    id="phoneNumber"
+                                    name="phoneNumber"
+                                    value={companyFormData.phoneNumber}
+                                    onChange={handleInputChange}
+                                />
+                                {companyFormErrors.phoneNumber && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <label>{t('mainSector')}</label>
+                                <select
+                                    className="form-select"
+                                    id="industry"
+                                    name="industry"
+                                    value={companyFormData.industry}
+                                    onChange={handleInputChange}
+                                >
+                                    <option value={companyFormData.industry}>{companyFormData.industry}</option>
+                                    <option value="Digital">{t('Digital')}</option>
+                                    <option value="Industry">{t('Industry')}</option>
+                                    <option value="Social and Solidarity Economy">{t('Social and Solidarity Economy')}</option>
+                                    <option value="Cultural and Creative Industry">{t('Cultural and Creative Industry')}</option>
+                                </select>
+                                {companyFormErrors.industry && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                            <div className="col">
+                                <label>{t('maturityProject')}</label>
+                                <select
+                                    className="form-select"
+                                    id="maturity"
+                                    name="maturity"
+                                    value={companyFormData.maturity}
+                                    onChange={handleInputChange}
+                                >
+                                    <option value={companyFormData.maturity}>{companyFormData.maturity}</option>
+                                    <option value="Ideation">{t('Ideation')}</option>
+                                    <option value="Minimum Viable Product">{t('Minimum Viable Product')}</option>
+                                    <option value="First Customer / First Users">{t('First Customer / First Users')}</option>
+                                    <option value="Commercialization">{t('Commercialization')}</option>
+                                    <option value="International">{t('International')}</option>
+                                </select>
+                                {companyFormErrors.maturity && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                            <div className="col">
+                                <label>{t('primarySector')}</label>
+                                <select
+                                    className="form-select"
+                                    id="primarySector"
+                                    name="primarySector"
+                                    value={companyFormData.primarySector}
+                                    onChange={handleInputChange}
+                                >
+                                    <option value={companyFormData.primarySector}>{companyFormData.primarySector}</option>
+                                    <option value="Banking / Insurance">{t('Banking / Insurance')}</option>
+                                    <option value="Construction">{t('Construction')}</option>
+                                    <option value="Chemical">{t('Chemical')}</option>
+                                    <option value="Medical Devices">{t('Medical Devices')}</option>
+                                    <option value="Biotechnology">{t('Biotechnology')}</option>
+                                    <option value="Commerce / Distribution">{t('Commerce / Distribution')}</option>
+                                    <option value="Event / Communication">{t('Event / Communication')}</option>
+                                    <option value="Pharmaceutical Industry">{t('Pharmaceutical Industry')}</option>
+                                    <option value="IT / Telecommunications">{t('IT / Telecommunications')}</option>
+                                    <option value="Textile / Clothing">{t('Textile / Clothing')}</option>
+                                    <option value="Transportation / Logistics">{t('Transportation / Logistics')}</option>
+                                    <option value="Sports and Leisure">{t('Sports and Leisure')}</option>
+                                    <option value="Tourism / Catering">{t('Tourism / Catering')}</option>
+                                    <option value="e-Health and Well-being">{t('e-Health and Well-being')}</option>
+                                    <option value="Education / Training">{t('Education / Training')}</option>
+                                    <option value="Energy">{t('Energy')}</option>
+                                    <option value="Legal">{t('Legal')}</option>
+                                    <option value="Digital Industry">{t('Digital Industry')}</option>
+                                    <option value="Business Services">{t('Business Services')}</option>
+                                </select>
+                                {companyFormErrors.maturity && (
+                                    <div className="invalid-feedback">City field is required</div>
+                                )}
+                            </div>
+                        </div>
+                        <div className="row mb-3">
+                            <div className="col">
+                                <label>{t('profilepicture')}</label>
                                 <input
                                     className="form-control"
                                     type="file"
